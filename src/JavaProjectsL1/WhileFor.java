ccc69203eaf6d32583222269129aacb982eb08e4
Mustafa-Deveci <m.deveci9@hotmail.com>
2021-07-08 16:20:09 +0300

    using while loop and for loop

---
+++ src/JavaProjectsL1/WhileFor.java
@@ -0,0 +1,16 @@
+package JavaProjectsL1;
+
+public class WhileFor {
+    public static void main(String[] args) {
+        int num = 10;
+        while (num <= 20) {
+            System.out.println("w while ");
+            num++;
+        }
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("w for ");
+
+        }
+
+    }
+}
