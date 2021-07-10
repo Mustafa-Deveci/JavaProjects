afbb8580b753bd791849d57956d45447146621fe
Mustafa-Deveci <m.deveci9@hotmail.com>
2021-07-10 23:20:14 +0300

    print properties of data types

---
+++ src/JavaProjectsL1/DataTypes.java
@@ -0,0 +1,15 @@
+package JavaProjectsL1;
+
+public class DataTypes {
+    public static void main(String[] args) {
+// data types used for integers
+        System.out.println(" Byte en küçük değeri : " + Byte.MIN_VALUE + " En büyük değeri : " + Byte.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Byte.SIZE);
+        System.out.println(" Short en küçük değeri : " + Short.MIN_VALUE + " En büyük değeri : " + Short.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Short.SIZE);
+        System.out.println(" Integer en küçük değeri : " + Integer.MIN_VALUE + "En büyük değeri : " + Integer.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Integer.SIZE);
+        System.out.println(" Long en küçük değeri : " + Long.MIN_VALUE + " En büyük değeri : " + Long.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Long.SIZE);
+
+// data types used for decimal numbers
+        System.out.println(" Float en küçük değeri : " + Float.MIN_VALUE + " En büyük değeri : " + Float.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Float.SIZE);
+        System.out.println(" Double en küçük değeri : " + Double.MIN_VALUE + " En büyük değeri : " + Float.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Double.SIZE);
+    }
+}
