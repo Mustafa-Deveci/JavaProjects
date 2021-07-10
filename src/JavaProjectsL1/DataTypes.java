f70ad008ffbee163b06fc4bdd6ad18b6ce3db014
Mustafa Deveci <m.deveci9@hotmail.com>
2021-07-10 23:25:52 +0300

    Update DataTypes.java

--- src/JavaProjectsL1/DataTypes.java
+++ src/JavaProjectsL1/DataTypes.java
@@ -3,13 +3,13 @@ package JavaProjectsL1;
 public class DataTypes {
     public static void main(String[] args) {
 // data types used for integers
-        System.out.println(" Byte en küçük değeri : " + Byte.MIN_VALUE + " En büyük değeri : " + Byte.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Byte.SIZE);
-        System.out.println(" Short en küçük değeri : " + Short.MIN_VALUE + " En büyük değeri : " + Short.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Short.SIZE);
-        System.out.println(" Integer en küçük değeri : " + Integer.MIN_VALUE + "En büyük değeri : " + Integer.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Integer.SIZE);
-        System.out.println(" Long en küçük değeri : " + Long.MIN_VALUE + " En büyük değeri : " + Long.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Long.SIZE);
+        System.out.println(" Byte min : " + Byte.MIN_VALUE + " max : " + Byte.MAX_VALUE + " byte (space used in memory : " + Byte.SIZE);
+        System.out.println(" Short min : " + Short.MIN_VALUE + " max : " + Short.MAX_VALUE + " byte (space used in memory : " + Short.SIZE);
+        System.out.println(" Integer min : " + Integer.MIN_VALUE + "max : " + Integer.MAX_VALUE + " byte (space used in memory : " + Integer.SIZE);
+        System.out.println(" Long min : " + Long.MIN_VALUE + " max : " + Long.MAX_VALUE + " byte (space used in memory : " + Long.SIZE);
 
 // data types used for decimal numbers
-        System.out.println(" Float en küçük değeri : " + Float.MIN_VALUE + " En büyük değeri : " + Float.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Float.SIZE);
-        System.out.println(" Double en küçük değeri : " + Double.MIN_VALUE + " En büyük değeri : " + Float.MAX_VALUE + " Kaç bit (bellekte kapladığı alan : " + Double.SIZE);
+        System.out.println(" Float min : " + Float.MIN_VALUE + " max : " + Float.MAX_VALUE + " byte (space used in memory : " + Float.SIZE);
+        System.out.println(" Double min : " + Double.MIN_VALUE + " max : " + Float.MAX_VALUE + " byte (space used in memory : " + Double.SIZE);
     }
 }
