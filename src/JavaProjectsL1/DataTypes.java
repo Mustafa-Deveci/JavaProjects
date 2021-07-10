85472b980c907200bc339642903de910843dec7e
Mustafa Deveci <m.deveci9@hotmail.com>
2021-07-10 23:44:44 +0300

    Update DataTypes.java

--- src/JavaProjectsL1/DataTypes.java
+++ src/JavaProjectsL1/DataTypes.java
@@ -3,13 +3,13 @@ package JavaProjectsL1;
 public class DataTypes {
     public static void main(String[] args) {
 // data types used for integers
-        System.out.println(" Byte min : " + Byte.MIN_VALUE + " max : " + Byte.MAX_VALUE + " byte (space used in memory : " + Byte.SIZE);
-        System.out.println(" Short min : " + Short.MIN_VALUE + " max : " + Short.MAX_VALUE + " byte (space used in memory : " + Short.SIZE);
-        System.out.println(" Integer min : " + Integer.MIN_VALUE + "max : " + Integer.MAX_VALUE + " byte (space used in memory : " + Integer.SIZE);
-        System.out.println(" Long min : " + Long.MIN_VALUE + " max : " + Long.MAX_VALUE + " byte (space used in memory : " + Long.SIZE);
+        System.out.println(" Byte min : " + Byte.MIN_VALUE + " max : " + Byte.MAX_VALUE + " byte (space used in memory) : " + Byte.SIZE);
+        System.out.println(" Short min : " + Short.MIN_VALUE + " max : " + Short.MAX_VALUE + " byte (space used in memory) : " + Short.SIZE);
+        System.out.println(" Integer min : " + Integer.MIN_VALUE + "max : " + Integer.MAX_VALUE + " byte (space used in memory) : " + Integer.SIZE);
+        System.out.println(" Long min : " + Long.MIN_VALUE + " max : " + Long.MAX_VALUE + " byte (space used in memory) : " + Long.SIZE);
 
 // data types used for decimal numbers
-        System.out.println(" Float min : " + Float.MIN_VALUE + " max : " + Float.MAX_VALUE + " byte (space used in memory : " + Float.SIZE);
-        System.out.println(" Double min : " + Double.MIN_VALUE + " max : " + Float.MAX_VALUE + " byte (space used in memory : " + Double.SIZE);
+        System.out.println(" Float min : " + Float.MIN_VALUE + " max : " + Float.MAX_VALUE + " byte (space used in memory) : " + Float.SIZE);
+        System.out.println(" Double min : " + Double.MIN_VALUE + " max : " + Float.MAX_VALUE + " byte (space used in memory) : " + Double.SIZE);
     }
 }
